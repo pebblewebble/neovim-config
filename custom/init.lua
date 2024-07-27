06938ccfa75666c8235df6174599c4dbac77a4c9
pebblewebble <ericlye123344@gmail.com>
2024-07-27 21:16:26 +0800

    Center cursor

--- custom/init.lua
+++ custom/init.lua
@@ -7,3 +7,14 @@
 -- })
 vim.wo.relativenumber = true
 vim.g.nvim_tree_respect_buf_cwd = 1
+
+
+local function center_cursor()
+  vim.api.nvim_feedkeys("zz", "n", true)
+end
+
+-- Map Ctrl-d (page down) and center the cursor
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+
+-- Map Ctrl-u (page up) and center the cursor
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
